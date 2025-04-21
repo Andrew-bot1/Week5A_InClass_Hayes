61a7ad7de6f5b532d19753a97acb04b1ab3691f4
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-04-20 22:49:11 -0500

    Update project08-01.js

--- Week5A_InClass_Hayes/project08-01.js
+++ Week5A_InClass_Hayes/project08-01.js
@@ -14,23 +14,46 @@
 //constructor
 function timer(min,sec){
       //declare
-      timer.minutes=min;
-      timer.seconds=sec;
-      timer.timeID=null;
+      this.minutes=min;
+      this.seconds=sec;
+      this.timeID=null;
 }
 
 //add method to see if timer is paused
 function runPause(timer, minBox, secBox) {
       //if statement to see if timer is paused
-      if (timer.timedID!=null) {
+      if (timer.timeID != null) {
             //pause timer
             window.clearInterval(timer.timeID);
-            timer.timeID=null;
-      }
+            //set timeid to null
+            timer.timeID = null;
+      } 
       else {
-            
+            //start timer
+            timer.timeID = window.setInterval(function countdown() {
+                  //see if seconds is greater than 0
+                  if (timer.seconds > 0) {
+                  //decrease seconds value by 1
+                  timer.seconds -= 1;
+                  }
+                  //see if mins is greater than 0
+                  else if (timer.minutes > 0) {
+                        //decrease mins by 1
+                        timer.minutes -= 1;
+                  }
+                  //stop timer
+                  else {
+                        window.clearInterval(timer.timeID);
+                        //set timeid to null
+                        timer.timeID = null;
+                  }
+                  //change value of minutes and seconds
+                  timer.minutes = minBox.value;
+                  timer.seconds = secBox.value;
+            }, 1000);
       }
 }
+
 
 
 
@@ -44,3 +67,18 @@ let minBox = document.getElementById("minutesBox");
 let secBox = document.getElementById("secondsBox");
 let runPauseTimer = document.getElementById("runPauseButton");
 
+//declare myTimer object
+let myTimer=new timer(minBox.value,secBox.value);
+//change minuetes to minBox value
+minBox.onchange=function(){
+      myTimer.minutes= minBox.value;
+}
+//change seconds to secBox value
+secBox.onchange=function(){
+      myTimer.seconds=secBox.value;
+}
+
+// pause timer  
+runPauseTimer.onclick=function(){
+      myTimer=runPause(myTimer,minBox,secBox);
+}
